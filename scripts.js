fbc574f9158606e0a3afee621fd5e0a44c5ee3a9
LucasCabral12 <lucascsmarques092@gmail.com>
2023-03-10 08:45:25 -0300

    Covert Money

---
+++ scripts.js
@@ -0,0 +1,70 @@
+
+const button = document.getElementById('convert-button')
+const select = document.getElementById('currency-select')
+
+const dolar = 5.2
+const euro = 5.9
+const bitcoin = 0.0000086
+
+
+const convertValues = () => {
+
+    const inputReais = document.getElementById('input-real').value
+    const realValueText = document.getElementById('real-value-text')
+    const currencyValueText = document.getElementById('currency-value-text')
+
+
+
+    realValueText.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: 'currency', currency: 'BRL'
+    }).format(inputReais)
+
+    if (select.value === "US$ Dólar Americano") {
+        currencyValueText.innerHTML = new Intl.NumberFormat("en-US", {
+            style: 'currency', currency: 'USD'
+        }).format(inputReais / dolar)
+    }
+
+    if (select.value === "€ Euro") {
+        currencyValueText.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: 'currency', currency: 'EUR'
+        }).format(inputReais / euro)
+    }
+
+    if (select.value === "Bitcoin") {
+        currencyValueText.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: 'currency', currency: 'Btc'
+        }).format(inputReais / bitcoin)
+    }
+
+
+    
+}
+
+changeCurrency = () => {
+    const currencyName = document.getElementById("currency-name")
+    const currencyimg = document.getElementById("currency-img")
+
+    if (select.value === '€ Euro') {
+        currencyName.innerHTML = "Euro"
+        currencyimg.src = "./img/Design sem nome 1.png"
+    }
+
+    if (select.value === 'US$ Dólar Americano') {
+        currencyName.innerHTML = "Dólar americano"
+        currencyimg.src = "./img/estados-unidos (1) 1.png"
+    }
+
+    if (select.value === 'Bitcoin') {
+        currencyName.innerHTML = "Bitcoin"
+        currencyimg.src = "./img/Design sem nome (1) 1.png"
+    }
+
+
+    convertValues()
+}
+
+
+button.addEventListener('click', convertValues)
+select.addEventListener('change', changeCurrency)
+
